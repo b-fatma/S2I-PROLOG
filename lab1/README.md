972429ef8a7169e57d58bda92a6013c8e7da281a
b-fatma <fz.boulgheraif@gmail.com>
2025-10-30 08:12:26 +0100

    session 2

--- lab1/README.md
+++ lab1/README.md
@@ -8,3 +8,18 @@ appartient(X, [X|_]).
 appartient(X, [_ , Y|L]) :- appartient(X, [Y|L]).
 ```
 ![alt text](exec/image.png)
+
+#### Q6
+`substitue(2, 6, [2, 3, 2, 6, 7, 8, 2, 9], L).`
+
+#### Q11
+```
+?- npair([1, 2, 3]).
+false.
+
+?- npair([1, 2]).
+true.
+
+?- npair([]).
+true.
+```
--- lab1/exo1.pl
+++ lab1/exo1.pl
@@ -13,6 +13,39 @@ supprimerK(1, [_|L], L).
 supprimerK(K, [X, Y|L1], [X|L2]) :- K2 is K-1, 
     supprimerK(K2, [Y|L1], L2).
 
-substitue(X, Y, [X|Lin], [Y|Lout]) :- !, 
-    substitute(X, Y, Lin, Lout).
-substitue(X, Y, [Z|Lin], [Z|Lout]) :- substitute(X, Y, Lin, Lout).
+substitue(_, _, [], []).
+substitue(X, Y, [X|Lin], [Y|Lout]) :- !,
+    substitue(X, Y, Lin, Lout).
+substitue(X, Y, [Z|Lin], [Z|Lout]) :- substitue(X, Y, Lin, Lout).
+
+longueur([], 0).
+longueur([_|L], K) :- longueur(L, K2),
+    K is K2 + 1.
+
+somme([X], X).
+somme([X, Y|L], S) :- somme([Y|L], S2),
+    S is S2 + X.
+
+affiche1([]).
+affiche1([X|L]) :- write(X),
+    nl,
+    affiche1(L).
+
+affiche2([]).
+affiche2([X|L]) :- affiche2(L),
+    nl,
+    write(X).
+
+npair([]).
+npair([_, _|L]) :- npair(L).
+
+present2fois(X, [X|L]) :- !,
+    appartient(X, L).
+present2fois(X, [Y|L]) :- present2fois(X, L).
+
+concat([], L2, L2).
+concat([X|L1], L2, [X|L3]) :- concat(L1, L2, L3).
+
+palindrome([X]).
+palindrome([]).
+palindrome([Y, L, Y]) :- palindrome(L).
